cccc404031ef3e74501734988d9d0f92a91726da
Bishwa Poudel <bispoul181@gmail.com>
2018-07-01 12:45:57 +0545

    Add user schema

--- api/models/User.js
+++ api/models/User.js
@@ -13,7 +13,27 @@ module.exports = {
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
+    name: {
+      type: 'string',
+      required: true,
+    },
+    email: {
+      type: 'email',
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: 'string',
+      required: true,
+      minLength: 8
+    },
+    // user_level 0 means admin
+    // user_level 1 means normal user
+    user_level: {
+      type: 'number',
+      in: [0,1],
+      defaultsTo: 1
+    }
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
