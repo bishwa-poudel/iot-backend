87b92f49a14b49f571cd81a91aa9485bc2150d10
Bishwa Poudel <bispoul181@gmail.com>
2018-07-05 17:00:53 +0545

    Create new reservation

--- api/controllers/ReservationController.js
+++ api/controllers/ReservationController.js
@@ -6,18 +6,69 @@
  * @author      :: https://github.com/beingbishwa
  */
 
-module.exports = {
+const moment = require('moment')
+
+const allowedParameters = ["start_time", "end_time", "_user"]
+
+const validateDate = (start, end, format="MM-DD-YYYY HH:mm:ss") => {
+  const start_time = moment(start, format, true)
+  const end_time = moment(end, format, true)
+
+  // check if date is in a given format
+  if(!start_time.isValid() || !end_time.isValid()){
+    return 'Error: Invalid date-time format'
+  }
   
+  // check isbefore
+  if(end_time.isBefore(start_time)){
+    return 'Error: Start-time should be before end-time'
+  }
+
+  return [start_time.unix(), end_time.unix()]
+}
+
+module.exports = {
+
 
   /**
    * `ReservationController.create()`
-   * 
    * @description :: Create a reservation for a device
+   * @route       :: POST /devices/:id/reserve
+   * @param start_time :: should be string in the form '2014-01-05 12:45:57'
+   * @param end_time :: should be string in a form '2014-01-05 12:45:57'
    */
   create: async function (req, res) {
-    return res.json({
-      todo: 'create() is not implemented yet!'
-    });
+    // get id from url
+    const deviceid = req.params.id
+
+    // check if such device exists
+    try {
+      if(await Device.count({id:deviceid}) == 0){
+        return ResponseService.json(400, res, 'No such device exists')
+      }
+    } catch (error) {
+      return ResponseService.json(500, res, 'Internal Server Error') 
+    }
+
+    // pick allowed parameter
+    const data = _.pick(req.body, allowedParameters)
+    data._device = deviceid
+
+    const validationResult = validateDate(data.start_time, data.end_time)
+
+    if(typeof(validationResult) == 'string'){
+      return ResponseService.json(400, res, validationResult)
+    }else if(typeof(validationResult) == 'object'){
+      [data.start_time, data.end_time] = validationResult
+    }
+
+    // create new reservation
+    try {
+      const reservation = await Reservation.create(data).fetch()
+      return ResponseService.json(201, res, 'Device reserved successfully', reservation)
+    } catch (error) {
+      res.json(error)
+    }
   },
 
   /**
